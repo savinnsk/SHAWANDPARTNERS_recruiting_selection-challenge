61e510cfa65389c9953e94e7dde48a2d8397c561
54vio <savio.document@gmail.com>
2024-03-23 09:08:40 -0300

    refactor: move send button to own component

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -1,41 +1,15 @@
 import s from "./header.module.css"
-import useStore from "../../hook/store";
 import logo from "../../../public/logo.png"
 import { useState } from "react";
-import {  sendFileService } from "../../services/api";
 import { Search } from "../search/search";
+import { SendButton } from "../send-button/send-button";
 
 export function Header() {
 
-  const store = useStore()
   
 
   const [eventInput , setEventInput] = useState<React.ChangeEvent<HTMLInputElement>>()
   const [fileName , setFileName] = useState<string>("")
-
-  const handleFileUpload = async (event :React.ChangeEvent<HTMLInputElement> | undefined) => {
-    let file  
-    const formData = new FormData();
-
-    if(event && event.target.files){
-      file = event.target.files![0];
-      formData.append('csvFile', file);
-      }
-      setFileName("")
-
-      const response = await sendFileService(formData)
-
-      if(response.status > 399){
-        store?.toSetNotification(response.data.message)
-        store?.toSetError()
-        return
-      }
-
-      store?.toSetNotification(response.data.message)
-   
-  };
-
-
 
 
     return (
@@ -58,7 +32,7 @@ export function Header() {
           )}
           
 
-          <button className={s.sendButton} onClick={() => handleFileUpload(eventInput)}>SEND</button>
+          <SendButton setFileName={setFileName} eventInput={eventInput}/>
         </div>
       </header>
     )
