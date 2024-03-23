6c1bd225311013d20025d54a38ac49e6c617f728
savinnsk <saviodocuments@gmail.com>
2024-03-22 21:16:14 -0300

    feat: add method to react csv from uploads | add csv parser

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,16 +1,68 @@
 import {Router , Request , Response} from "express"
 import { upload } from "./config/multer"
-
+import fs from "fs";
+import csvParser from "csv-parser";
 
 const routes = Router()
 
 
 routes.post("/files", upload.single('csvFile'),(req : Request ,res : Response)=>{
-    return res.status(200)
+      try {
+        return res.status(200).json({ message: "The file was uploaded successfully." });
+      } catch (error) {
+        console.error("Error uploading file:", error);
+        return res.status(500).json({ message: "An error occurred while uploading the file." });
+      }
 })
 
-routes.get("/users", (req : Request , res : Response)=>{
-    return res.status(200).json({message : "users"})
-})
+routes.get("/users", async (req: Request, res: Response) => {
+    try {
+      const query = req.query.q?.toString().toLowerCase();
+  
+      if (!query) {
+        return res.status(400).json({ message: "Search query parameter 'q' is required." });
+      }
+  
+      const searchResults: any[] = [];
+      const csvFolder = "./uploads";
+  
+      const files = fs.readdirSync(csvFolder).filter(file => file.endsWith(".csv"));
+  
+      for (const file of files) {
+        const filePath = `${csvFolder}/${file}`;
+        const fileData = await readCSVFile(filePath);
+  
+        fileData.forEach((row: any) => {
+          const value = row[query];
+          if (value) {
+            searchResults.push({value, file : filePath});
+          }
+        });
+      }
+  
+      return res.status(200).json({ data: searchResults });
+    } catch (error) {
+      console.error("Error searching data:", error);
+      return res.status(500).json({ message: "An error occurred while searching data." });
+    }
+  });
+  
 
+  async function readCSVFile(filePath: string): Promise<any[]> {
+    return new Promise((resolve, reject) => {
+      const results: any[] = [];
+  
+      fs.createReadStream(filePath)
+        .pipe(csvParser())
+        .on("data", (row: any) => {
+          results.push(row);
+        })
+        .on("end", () => {
+          resolve(results);
+        })
+        .on("error", (error: any) => {
+          reject(error);
+        });
+    });
+  }
 export {routes} 
