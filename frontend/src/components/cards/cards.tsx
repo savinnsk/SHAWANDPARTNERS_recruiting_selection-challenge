4171218cbd40c26411ff8a06ad528e6e9456774a
54vio <savio.document@gmail.com>
2024-03-23 19:09:25 -0300

    refactor: color message card

--- frontend/src/components/cards/cards.tsx
+++ frontend/src/components/cards/cards.tsx
@@ -8,11 +8,11 @@ export function Cards(){
 
     return (
         <div className={s.cardsContainer}>
-            {cards?.length == 0 && <p style={{color: "#7daa4e"}} >find values by search bar to display here</p>}
+            {cards?.length == 0 && <p style={{color: "#121214"}} >find values by search bar to display here</p>}
             {cards?.map((card, index) => {
                 return (
                     <div key={index} className={s.card}>
-                        <span style={{color: "#9dc86e"}}>matching with : {card.value}</span>
+                        <span style={{color: "#131a0b"}}>matching with : {card.value}</span>
                         {Object.entries(card.other).map(([key, value]) => (
                             <div key={key}>
                                 <span>{key}: </span>
